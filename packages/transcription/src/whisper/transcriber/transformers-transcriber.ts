4d010260af088e531fa359d749322ac9a12bada0
lutangar <johan@larriereguichet.fr>
2024-04-19 14:09:33 +0200

    chore: add ctranslate2 and timestamped

--- packages/transcription/src/whisper/transcriber/transformers-transcriber.ts
+++ packages/transcription/src/whisper/transcriber/transformers-transcriber.ts
@@ -1,6 +1,8 @@
 import { TranscriptionModel } from '../../transcription-model.js'
 import { AbstractTranscriber } from '../../abstract-transcriber.js'
 import { Transcript, TranscriptFormat } from '../../transcript.js'
+import { $ } from 'execa'
+import { join } from 'path'
 
 export class TransformersTranscriber extends AbstractTranscriber {
   async transcribe (
@@ -9,6 +11,33 @@ export class TransformersTranscriber extends AbstractTranscriber {
     language: string,
     format: TranscriptFormat = 'vtt'
   ): Promise<Transcript> {
-    return Promise.resolve(undefined)
+    const $$ = $({ verbose: true })
+    // const ffmpegChildProcess = $$`ffmpeg ${[
+    //   '-i',
+    //   mediaFilePath,
+    //   '-vn', // no video
+    //   '-ar',
+    //   16000, // set the audio sampling frequency
+    //   '-ac',
+    //   '1', // set the number of audio channels to 1 since Vosk is expecting mono
+    //   '-bufsize',
+    //   1000, // set a buffer size to provide a steady flow of frames
+    //   '-'
+    // ]}`
+
+    await $$`transformers-cli ${[
+      '--task',
+      'automatic-speech-recognition',
+      '--model',
+      'openai/whisper-tiny',
+      '--input',
+      mediaFilePath
+    ]}`
+
+    return {
+      language,
+      path: join(this.transcriptDirectory, `test.${format}`),
+      format
+    }
   }
 }
